281390e24bfe580e8e6d73e221c585b7bd38705c
Splatterbot <king_ben_i@yahoo.com>
2023-02-27 10:52:16 -0600

    Added Back Air Temp
    Air Temp Still Does not work, but it at least is now formatted nicer in the output.
    
    Water sensor was unaffected by this addition and is still fully operational.

--- Arduino/DataCollectionMain/DataCollectionMain.ino
+++ Arduino/DataCollectionMain/DataCollectionMain.ino
@@ -5,34 +5,63 @@ Copyright (c) 2011, Matt Sparks
 All rights reserved.
 */
 
+
 #include <stdlib.h>
 #include <DS1620.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+ float Celcius=0;
+ float Fahrenheit=0;
 
 
 // Set the appropriate digital I/O pin connections.
-// See the datasheet for more details.
+// See the datasheet f  qor more details.
 static const uint8_t RST_PIN = 7;  // pin 3 on DS1620
 static const uint8_t CLK_PIN = 8;  // pin 2 on DS1620
 static const uint8_t DQ_PIN  = 9;  // pin 1 on DS1620
 
 
 DS1620 ds1620(RST_PIN, CLK_PIN, DQ_PIN);
+
 void setup()
 {
+
   Serial.begin(9600); // Baud rate
   delay(100);
 
   ds1620.config();
+
+  Serial.begin(9600);
+  sensors.begin();
 }
 
 
 void loop()
 {
+
   const float temp_c = ds1620.temp_c();
   const float temp_f = temp_c * 9/5.0 + 32;
 
-  Serial.print(temp_f, 1);  // 1 decimal place
-  Serial.print("F ");
+  Serial.println("Air Temp");
+  Serial.print(" C  ");
   Serial.print(temp_c, 1);
-  Serial.println("C");
+  Serial.print(" F  ");
+  Serial.println(temp_f, 1);  // 1 decimal place
+
+  Serial.println("Water Temp");
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  delay(1000);
 }
