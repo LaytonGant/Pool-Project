f7bc0d28022b53524fe54c3cea3c5d4b29fcabaf
BoneBot <cmcbride0304@gmail.com>
2023-04-06 10:53:59 -0500

    Added all checkboxes to form object

--- Website Code/Website.py
+++ Website Code/Website.py
@@ -4,26 +4,33 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    if request.method == "POST":
-       app.logger.info("Post request")
-       chckbxVal = request.form["chkbx1"]
-       app.logger.info("Checkbox value:"+chckbxVal)
-    else:
-        app.logger.info("Get request")
+    # Initialize variables
     waterLevel = "12"
     waterTemperature = "100"
     airTemperature = "100"
     phLevel = "7"
-    return render_template("Website.html", waterLevel=waterLevel , waterTemperature=waterTemperature,
-                          airTemperature=airTemperature, phLevel=phLevel)
-
-@app.route("/login", methods=["POST","GET"])
-def login():
+    
+    # Check request type
+    # POST
     if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+       app.logger.info("Post request")
+       postFunc("pumpSwitch")
+       postFunc("lightsSwitch")
+       postFunc("filterSwitch")
+    
+    # GET
     else:
-        return render_template("login.html")
+        app.logger.info("Get request")
+    
+    # Render template
+    return render_template("Website.html", waterLevel=waterLevel , waterTemperature=waterTemperature,
+                        airTemperature=airTemperature, phLevel=phLevel)
+
+def postFunc(elem):
+    if elem in request.form:
+        formVal = request.form[elem]
+        app.logger.info("Checkbox value:"+formVal)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
