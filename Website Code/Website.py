b7a525273c89ebb8fb3c3b717c8e70ee39f63e47
BoneBot <cmcbride0304@gmail.com>
2023-04-19 17:10:37 -0500

    Added home page data req and started temp page

--- Website Code/Website.py
+++ Website Code/Website.py
@@ -1,30 +1,70 @@
 from flask import Flask, redirect, url_for, render_template, request
 import datetime
+from PoolManager import *
 
 app = Flask(__name__)
 
 scheduleTimes = [[0] * 2 for _ in range(4)] #column 0 is off column 1 is on, range 0 is lights, range 1 is filter, range 2 is pump, range 3 is temperature
 tempPass = 0 #temperature being passed
 
-@app.route("/")
+def startPoolManager():
+   if not PoolManager.isInit:
+      PoolManager.initialize()
+
+# Home page
+@app.route("/", methods=["GET", "POST"])
 def home():
-   waterLevel = "12"
-   waterTemperature = "100"
-   airTemperature = "100"
-   phLevel = "7"
+   # Initialize Pool Manager
+   startPoolManager()
+
+   # Get full status
+   status = PoolManager.reqFullStatus()
+
+   # Set values
+   waterLevel = status["WaterLevel"]
+   waterTemperature = status["WaterTemp"]
+   airTemperature = status["AirTemp"]
+   phLevel = status["pH"]
+   
    return render_template("home.html", waterLevel=waterLevel , waterTemperature=waterTemperature,
                           airTemperature=airTemperature, phLevel=phLevel)
 
+# Devices page
 @app.route("/devices")
 def devices():
+   # Initialize pool manager
+   startPoolManager()
+
+   # Render webpage
    return render_template("devices.html", scheduleTimes=scheduleTimes)
 
-@app.route("/temperature")
+# Temperature page
+@app.route("/temperature", methods=["GET", "POST"])
 def temperature():
-   return render_template("temperature.html", scheduleTimes=scheduleTimes, tempPass=tempPass)
+   # Initialize pool manager
+   startPoolManager()
 
+   # Retrieve water temperature
+   waterTemperature = PoolManager.reqStatus("WaterTemp")
+
+   # Temperature change request
+   if request.method == "POST":
+      # If temperature is higher, turn on heater
+      if request.form["temperatureValue"] > waterTemperature:
+         PoolManager.setStatus("Heater",1)
+      else:
+         PoolManager.setStatus("Heater",0)
+
+   # Render webpage
+   return render_template("temperature.html", waterTemperature=waterTemperature, scheduleTimes=scheduleTimes, tempPass=tempPass)
+
+# Schedule page
 @app.route("/schedule")
 def schedule():
+   # Initialize pool manager
+   startPoolManager()
+
+   # Render webpage
    return render_template("schedule.html", scheduleTimes=scheduleTimes, tempPass=tempPass)
 
 #@app.route("/login", methods=["POST","GET"])
@@ -36,4 +76,7 @@ def schedule():
 #        return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='5000')
+   # Run as private server
+   app.run(debug=True)
+   # Run as public server
+   #app.run(debug=True, host='0.0.0.0', port='5000')
